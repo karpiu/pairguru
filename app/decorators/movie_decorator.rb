9e7fa2e98e0b18269fbf36bcb98257812eaf442f
Michal Karpinski <karp0506@gmail.com>
2020-01-11 11:57:48 +0100

    Task1: fetch data from external api

--- app/decorators/movie_decorator.rb
+++ app/decorators/movie_decorator.rb
@@ -2,8 +2,20 @@ class MovieDecorator < Draper::Decorator
   delegate_all
 
   def cover
-    "http://lorempixel.com/100/150/" +
-      %w[abstract nightlife transport].sample +
-      "?a=" + SecureRandom.uuid
+    MovieDetails::BASE_URL + details[:image_url]
+  end
+
+  def plot
+  	details[:plot]
+  end
+
+  def rating
+  	details[:rating]
+  end
+
+  private
+
+  def details
+  	@details ||= MovieDetails.new.call(object.title)
   end
 end
